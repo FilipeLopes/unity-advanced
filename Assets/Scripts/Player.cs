fbbb08c61725b9cc9af915bdf2330531f0494fb4
lopesfiliper <lopes.filiper@gmail.com>
2023-07-06 13:43:00 +0200

    Created the animation and the script to control it

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 7f;   
-    
+    [SerializeField] private float moveSpeed = 7f;
+    private bool isWalking;
 
     private void Update()
     {
@@ -30,8 +30,16 @@ public class Player : MonoBehaviour
         inputVector = inputVector.normalized;
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+        isWalking = moveDir != Vector3.zero;
+
         //It makes the character look to the correct direction. Using Slerp makes the transition smoother.
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward,moveDir,Time.deltaTime*rotateSpeed);
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 }
