4dde9ff459e8d442a03b9f2180f1740978a359bc
lopesfiliper <lopes.filiper@gmail.com>
2023-07-06 17:06:24 +0200

    Created interactions bettween player and objects

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,28 +6,69 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private GameInput gameInput;
+    [SerializeField] private LayerMask countersLayerMask;
 
     private bool isWalking;
+    private Vector3 lastInteractDir;
 
     private void Update()
     {
+        HandleMovement();
+        HandleInteractions();
+    }
+
+    private void HandleInteractions()
+    {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
-        
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        //It is used to save the last player direction, otherwise player will only detect collision with objects when moving.
+        if (moveDir != Vector3.zero)
+        {
+            lastInteractDir = moveDir;
+        }
+
+        float interactDistance = 2f;
+
+        //Creates a raycast and if it hits something will return it inside of rayCastHit variable. If it's true, goes to inside of the if.
+        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit rayCastHit, interactDistance, countersLayerMask)) 
+        {
+            //Gets the return of out RaycastHit and try it to identify if the component hit has the ClearCounter class inside of it.
+            if (rayCastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                //Has ClearCounter
+                clearCounter.Interact();
+            }
+        }
+        else
+        {
+            Debug.Log("-");
+        }
+
+    }
+
+    private void HandleMovement()
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
 
         float moveDistance = moveSpeed * Time.deltaTime;
         float playerRadius = .7f;
         float playerHeight = 2f;
+
+        //Creates a capsule collider around the player and test if player can go to an specific direction.
         bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
 
-        if (!canMove) 
+        //check if can move.
+        if (!canMove)
         {
             //Cannot move towards moveDir
 
             //Attempt only X movement
             Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
             canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
-            if(canMove)
+            if (canMove)
             {
                 //Can move only on the X
                 moveDir = moveDirX;
@@ -39,8 +80,8 @@ public class Player : MonoBehaviour
                 //Attempt only Z movement
                 Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
                 canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
-                
-                if(canMove )
+
+                if (canMove)
                 {
                     //Can move only on the Z
                     moveDir = moveDirZ;
@@ -53,20 +94,22 @@ public class Player : MonoBehaviour
             }
         }
 
+        //Check if player can move, if so, make it move.
         if (canMove)
         {
             transform.position += moveDir * moveDistance;
         }
 
+        //Check if moveDir is different from zero, if so, set isWalking as true.
         isWalking = moveDir != Vector3.zero;
 
         //It makes the character look to the correct direction. Using Slerp makes the transition smoother.
         float rotateSpeed = 10f;
-        transform.forward = Vector3.Slerp(transform.forward,moveDir,Time.deltaTime*rotateSpeed);
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
     }
-
     public bool IsWalking()
     {
         return isWalking;
     }
+
 }
